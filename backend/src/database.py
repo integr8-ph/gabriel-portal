69d4be2bd0d81e16c27b622c403ba8492a02f9dc
jessie-integr8ph <jessie@integr8.uk>
2024-02-25 16:51:31 +0800

    changed on_startup -> lifespan

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,3 +1,7 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from fastapi import FastAPI
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
@@ -5,8 +9,10 @@ from src.config import get_settings
 from src.models import gather_models
 
 
-async def db_init() -> None:
+@asynccontextmanager
+async def db_init(app: FastAPI) -> AsyncGenerator[None, None]:
     client = AsyncIOMotorClient(get_settings().DATABASE_URL)
     await init_beanie(
         client[get_settings().COLLECTION_NAME], document_models=gather_models()
     )
+    yield
