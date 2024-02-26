df2a67dfb2358d7ea7c55b01173c9089cae8c43f
jessie-integr8ph <jessie@integr8.uk>
2024-02-26 11:02:59 +0800

    moved test_db URL to .env file and refactored loading env files

--- backend/tests/users/conftest.py
+++ backend/tests/users/conftest.py
@@ -11,25 +11,18 @@ from src.models import gather_models
 from src.config import get_settings
 from src.auth.dependencies import get_current_active_superuser
 
-from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 
-CURRENT_DIRECTORY = Path(__file__).resolve().parent.parent
-TEST_DATABASE_URL = "mongodb+srv://yukokunomoriarty:xmsJjbqpSQ0h1X6y@home-security-neko.ovbtlac.mongodb.net/"
+load_dotenv(find_dotenv())
 
-try:
-    load_dotenv(CURRENT_DIRECTORY / ".env")
-except Exception:
-    load_dotenv(find_dotenv())
-
-os.environ["DATABASE_URL"] = TEST_DATABASE_URL
+os.environ["DATABASE_URL"] = os.getenv("TEST_DATABASE_URL")
 
 from src.main import app  # noqa
 
 
 @pytest_asyncio.fixture(autouse=True)
 async def clear_test_database():
-    client = AsyncIOMotorClient(TEST_DATABASE_URL)
+    client = AsyncIOMotorClient(os.getenv("TEST_DATABASE_URL"))
     test_db = client[get_settings().COLLECTION_NAME]
 
     await init_beanie(test_db, document_models=gather_models())
