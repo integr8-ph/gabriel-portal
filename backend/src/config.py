65a3215def51a47c9904ffdc0b491f7671096e3d
jessie-integr8ph <jessie@integr8.uk>
2024-02-26 11:48:06 +0800

    refactored

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 class Settings(BaseSettings):
     PROJECT_NAME: str
     DATABASE_URL: str
+    TEST_DATABASE_URL: str
     COLLECTION_NAME: str
     SECRET_KEY: str
     ALGORITHM: str
@@ -15,6 +16,9 @@ class Settings(BaseSettings):
         extra = "allow"
 
 
+settings = Settings()
+
+
 @lru_cache
 def get_settings() -> Settings:
-    return Settings()
+    return settings
