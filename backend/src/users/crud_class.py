761c44c8617d25ba45c7b7103fd5a05701bc8b9d
jessie-integr8ph <jessie@integr8.uk>
2024-02-27 14:26:36 +0800

    v2 of class-based CRUD

--- backend/src/users/crud_class.py
+++ backend/src/users/crud_class.py
@@ -1,10 +1,12 @@
 from typing import Any, Generic, Optional, Type, TypeVar  # noqa
-from beanie import Document  # noqa
 from bson import ObjectId  # noqa
 from fastapi.encoders import jsonable_encoder  # noqa
 
-from pydantic import BaseModel, EmailStr  # noqa
+from beanie import Document  # noqa
+from pydantic import BaseModel, EmailStr
 
+from src.exceptions import Conflict
+from src.users.exceptions import UserNotFound
 from src.auth.services import get_hashed_password  # noqa
 
 
@@ -35,25 +37,30 @@ class CRUDBase(Generic[ModelType]):
         self.model = model
 
     async def get(self, email: EmailStr) -> Optional[ModelType]:
-        return await self.model.find_one({"email": email})
+        user = await self.model.find_one({"email": email})
+        if not user:
+            raise UserNotFound()
+
+        return user
 
     async def get_all(self) -> Optional[list[ModelType]]:
         return await self.model.find_all().to_list()
 
     async def create(self, schema: BaseModel) -> Optional[ModelType]:
+        if await self.model.find_one({"email": schema.email}):
+            raise Conflict()
+
         created_user = await self.model(
             **schema.model_dump(exclude=["password"]), hashed_password=schema.password
         ).insert()
         return jsonable_encoder(created_user)
 
     async def update(self, email: EmailStr, schema: BaseModel) -> Optional[ModelType]:
-        await self.model.find_one({"email": email}).set(
-            {**schema.model_dump(exclude_none=True)}
-        )
-        updated_user = await self.get(email=email)
+        user = await self.get(email=email)
+        updated_user = await user.set({**schema.model_dump(exclude_none=True)})
         return jsonable_encoder(updated_user)
 
     async def delete(self, email: EmailStr) -> Optional[ModelType]:
-        deleted_user = await self.get(email=email)
-        await deleted_user.delete()
-        return jsonable_encoder(deleted_user)
+        user = await self.get(email=email)
+        await user.delete()
+        return jsonable_encoder(user)
