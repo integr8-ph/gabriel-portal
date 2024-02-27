66619b6f0907ca2cc5644e7d389160656a994c6d
jessie-integr8ph <jessie@integr8.uk>
2024-02-27 18:07:18 +0800

    refactored based on new class-based CRUD

--- backend/src/users/services.py
+++ backend/src/users/services.py
@@ -12,21 +12,21 @@ from src.users.schemas import (
     UserUpdate,
 )
 
-from src.users.crud_class import CRUDBase
+from src.users.crud_user import user
 
 
 async def get_user_by_email(email: EmailStr) -> Optional[User]:
-    return await CRUDBase(User).get(email)
+    return await user.get(key="email", value=email)
 
 
 async def get_all_users_in_db() -> Optional[CreateOut]:
-    return await CRUDBase(User).get_all()
+    return await user.get_all()
 
 
-async def create_user_in_db(user: UserCreate) -> CreateOut:
-    user.password = get_hashed_password(user.password)
+async def create_user_in_db(create_user: UserCreate) -> CreateOut:
+    create_user.hashed_password = get_hashed_password(create_user.hashed_password)
 
-    new_user = await CRUDBase(User).create(user)
+    new_user = await user.create(key="email", value=user.email, schema=create_user)
     return new_user
 
 
@@ -34,10 +34,10 @@ async def update_user_in_db(email: EmailStr, details: UserUpdate) -> UpdateOut:
     if details.hashed_password is not None:
         details.hashed_password = get_hashed_password(details.hashed_password)
 
-    updated_user = await CRUDBase(User).update(email, details)
+    updated_user = await user.update(key="email", value=email, schema=details)
     return updated_user
 
 
 async def delete_user_in_db(email: EmailStr) -> DeleteOut:
-    deleted_user = await CRUDBase(User).delete(email)
+    deleted_user = await user.delete(key="email", value=email)
     return deleted_user
