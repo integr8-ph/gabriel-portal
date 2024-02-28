9dd47be0bf88ae6ded8851380eb32084ae5f5cdb
jessie-integr8ph <jessie@integr8.uk>
2024-02-28 13:00:49 +0800

    removed non-db related logic and changed function parameter from BaseModel->Document

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,10 +1,7 @@
 from typing import Generic, Type, TypeVar, Optional, Any
 
 from beanie import Document
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-
-from src.exceptions import Conflict, NotFound
 
 ModelType = TypeVar("ModelType", bound=Document)
 
@@ -14,36 +11,18 @@ class CRUDBase(Generic[ModelType]):
         self.model = model
 
     async def get(self, key: str, value: Any) -> Optional[ModelType]:
-        doc = await self.model.find_one({key: value})
-        if not doc:
-            raise NotFound()
-
-        return doc
+        return await self.model.find_one({key: value})
 
     async def get_all(self) -> Optional[list[ModelType]]:
         return await self.model.find_all().to_list()
 
-    async def create(
-        self, key: str, value: Any, schema: BaseModel
-    ) -> Optional[ModelType]:
-        if await self.model.find_one({key: value}):
-            raise Conflict()
+    async def create(self, schema: BaseModel) -> Optional[ModelType]:
+        return await self.model(**schema.model_dump()).insert()
 
-        created_doc = await self.model(**schema.model_dump()).insert()
-        return jsonable_encoder(created_doc)
-
-    async def update(
-        self, key: str, value: Any, schema: BaseModel
-    ) -> Optional[ModelType]:
-        doc = await self.get(key=key, value=value)
-
-        updated_doc = await doc.set(
-            {**schema.model_dump(exclude_none=True, exclude_unset=True)}
+    async def update(self, details: BaseModel, doc: Document) -> Optional[ModelType]:
+        return await doc.set(
+            {**details.model_dump(exclude_none=True, exclude_unset=True)}
         )
-        return jsonable_encoder(updated_doc)
 
-    async def delete(self, key: str, value: Any) -> Optional[ModelType]:
-        deleted_doc = await self.get(key=key, value=value)
-
-        await deleted_doc.delete()
-        return jsonable_encoder(deleted_doc)
+    async def delete(self, doc: Document) -> Optional[ModelType]:
+        await doc.delete()
--- backend/src/users/crud_user.py
+++ backend/src/users/crud_user.py
@@ -1,21 +1,15 @@
-from typing import Optional, Any, TypeVar
+from typing import TypeVar
 
 from beanie import Document
 
 from src.users.models import User
-from src.users.exceptions import UserNotFound
 from src.crud import CRUDBase
 
 ModelType = TypeVar("ModelType", bound=Document)
 
 
 class CRUDUser(CRUDBase):
-    async def get(self, key: str, value: Any) -> Optional[ModelType]:
-        doc = await self.model.find_one({key: value})
-        if not doc:
-            raise UserNotFound()
-
-        return doc
+    pass
 
 
-user = CRUDUser(User)
+user_crud = CRUDUser(User)
