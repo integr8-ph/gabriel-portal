b37052c5bee51eac8c5aece11b99db347040929f
jessie-integr8ph <jessie@integr8.uk>
2024-02-29 09:42:50 +0800

    added cors middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database import db_init
 from src.api import router
 from src.config import get_settings
 
 app = FastAPI(lifespan=db_init, title=get_settings().PROJECT_NAME)
+
+app.add_middleware(CORSMiddleware, allow_origins="*", allow_methods="*")
 
 app.include_router(router)
